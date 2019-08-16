f59289603b960a6ee00ed2571f662b4fe457bfdd
tada <tada3@outlook.com>
2019-08-17 08:22:13 +0900

    cleaned up.

--- src/main.rs
+++ src/main.rs
@@ -117,82 +117,6 @@ fn calc(h: usize, w: usize, masu: &Vec<Vec<char>>) -> i32 {
         return 0;
     }
 
-    // Initialization (f = 0)
-    /*
-    for r1 in 0..h {
-        for r2 in r1..h {
-            for c1 in 0..w {
-                let mut c2 = c1 as i32;
-                if r2 == r1 {
-                    if c1 < w - 1 {
-                        let v = masu[r1][c1];
-                        if c1 > 0 && v == masu[r1][c1 - 1] {
-                            c2 = dp1[0][r1][r2][c1 - 1];
-                        } else {
-                            while c2 < (w - 1) as i32 && masu[r1][(c2 + 1) as usize] == v {
-                                c2 += 1;
-                            }
-                        }
-                    }
-                } else {
-                    let c2_prev = dp1[0][r1][r2 - 1][c1];
-                    let v_prev = masu[r2 - 1][c1];
-                    if c2_prev < 0 || masu[r2][c1] != v_prev {
-                        c2 = -1;
-                    } else {
-                        let c2_prev = dp1[0][r1][r2 - 1][c1];
-                        while c2 + 1 <= c2_prev && masu[r2][(c2 + 1) as usize] == v_prev {
-                            c2 += 1;
-                        }
-                    }
-                }
-                dp1[0][r1][r2][c1] = c2;
-                println!("dp1[{}][{}][{}][{}] = {}", 0, r1, r2, c1, c2);
-            }
-        }
-    }
-
-    if dp1[0][0][h - 1][0] == (w - 1) as i32 {
-        return 0;
-    }
-
-    for c1 in 0..w {
-        for c2 in c1..w {
-            for r1 in 0..h {
-                let mut r2 = r1 as i32;
-                if c2 == c1 {
-                    if r1 < h - 1 {
-                        let v = masu[r1][c1];
-                        if r1 > 0 && v == masu[r1 - 1][c1] {
-                            r2 = dp2[0][c1][c2][r1 - 1];
-                        } else {
-                            while r2 < (h - 1) as i32 && masu[(r2 + 1) as usize][c1] == v {
-                                r2 += 1;
-                            }
-                        }
-                    }
-                } else {
-                    let r2_prev = dp2[0][c1][c2 - 1][r1];
-                    let v_prev = masu[r1][c2 - 1];
-                    if r2_prev < 0 || masu[r1][c2] != v_prev {
-                        r2 = -1;
-                    } else {
-                        while r2 + 1 <= r2_prev && masu[(r2 + 1) as usize][c2] == v_prev {
-                            r2 += 1;
-                        }
-                    }
-                }
-                dp2[0][c1][c2][r1] = r2;
-                println!("dp2[{}][{}][{}][{}] = {}", 0, c1, c2, r1, r2);
-            }
-        }
-    }
-
-    if dp2[0][0][w - 1][0] == (h - 1) as i32 {
-        return 0;
-    }
-    */
-
     // DP
     println!("DP");
     for f in 1..max_f {
@@ -201,13 +125,13 @@ fn calc(h: usize, w: usize, masu: &Vec<Vec<char>>) -> i32 {
                 for c1 in 0..w {
                     println!("XXX {}, {}, {}, {}", f, r1, r2, c1);
                     // Tate
-                    let c2_tate = tate_wari2(w, &dp1, r1, r2, c1, f);
+                    let c2_tate = tate_wari(w, &dp1, r1, r2, c1, f);
 
                     let mut c2_yoko = c2_tate;
 
                     while c2_yoko < w as i32 {
                         println!("XXX c2_yoko = {}", c2_yoko);
-                        let r2_next = yoko_wari2(h, &dp2, c1, (c2_yoko + 1) as usize, r1, f);
+                        let r2_next = yoko_wari(h, &dp2, c1, (c2_yoko + 1) as usize, r1, f);
                         println!("XXX r2_next = {}", r2_next);
                         if r2_next >= r2 as i32 {
                             c2_yoko += 1;
@@ -234,10 +158,10 @@ fn calc(h: usize, w: usize, masu: &Vec<Vec<char>>) -> i32 {
             for c2 in c1 + 1..w + 1 {
                 for r1 in 0..h {
                     // Yoko
-                    let r2_yoko = yoko_wari2(h, &dp2, c1, c2, r1, f);
+                    let r2_yoko = yoko_wari(h, &dp2, c1, c2, r1, f);
                     let mut r2_tate = r2_yoko;
                     while r2_tate < h as i32 {
-                        let c2_next = tate_wari2(w, &dp1, r1, (r2_tate + 1) as usize, c1, f);
+                        let c2_next = tate_wari(w, &dp1, r1, (r2_tate + 1) as usize, c1, f);
                         if c2_next >= c2 as i32 {
                             r2_tate += 1;
                             continue;
@@ -260,12 +184,6 @@ fn calc(h: usize, w: usize, masu: &Vec<Vec<char>>) -> i32 {
         }
     }
 
-    //for f in 0..max_f {
-    //    if dp1[0][h - 1][0][f] == (w - 1) as i32 {
-    //        return f as i32;
-    //    }
-    //}
-    // if h = 1, w = 1
     println!("Fallback");
     return 0;
 }
@@ -289,32 +207,7 @@ fn get_4d_vec(s1: usize, s2: usize, s3: usize, s4: usize) -> Vec<Vec<Vec<Vec<i32
     return v1;
 }
 
-/**
 fn tate_wari(
-    w: usize,
-    dp1: &[Vec<Vec<Vec<i32>>>],
-    r1: usize,
-    r2: usize,
-    c1: usize,
-    f: usize,
-) -> i32 {
-    let c_x = dp1[(f - 1) % 2][r1][r2][c1];
-    if c_x < 0 {
-        -1
-    } else if c_x >= (w - 1) as i32 {
-        (w - 1) as i32
-    } else {
-        let c_xx = dp1[(f - 1) % 2][r1][r2][(c_x + 1) as usize];
-        if c_xx < 0 {
-            c_x
-        } else {
-            c_xx
-        }
-    }
-}
-*/
-
-fn tate_wari2(
     w: usize,
     dp1: &[Vec<Vec<Vec<i32>>>],
     r1: usize,
@@ -339,32 +232,7 @@ fn tate_wari2(
     }
 }
 
-/*
 fn yoko_wari(
-    h: usize,
-    dp2: &[Vec<Vec<Vec<i32>>>],
-    c1: usize,
-    c2: usize,
-    r1: usize,
-    f: usize,
-) -> i32 {
-    let r_x = dp2[(f - 1) % 2][c1][c2][r1];
-    if r_x < 0 {
-        -1
-    } else if r_x >= (h - 1) as i32 {
-        (h - 1) as i32
-    } else {
-        let r_xx = dp2[(f - 1) % 2][c1][c2][(r_x + 1) as usize];
-        if r_xx < 0 {
-            r_x
-        } else {
-            r_xx
-        }
-    }
-}
-*/
-
-fn yoko_wari2(
     h: usize,
     dp2: &[Vec<Vec<Vec<i32>>>],
     c1: usize,
